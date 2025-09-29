4e0d1ea5798eff2ad4d8b3010a2e6aa37cbc3a4e
Karl Murray <karl@karlm.dev>
2025-09-28 20:42:23 -0500

    Fix autofocus on the command

--- resources/views/livewire/components/command.blade.php
+++ resources/views/livewire/components/command.blade.php
@@ -17,7 +17,7 @@ new class extends Component {
     <flux:modal class="min-h-120 w-full max-w-120 px-6" name="search" variant="bare"
         x-on:keydown.cmd.k.document="$el.showModal()">
         <flux:command class="border-none shadow-lg">
-            <flux:command.input placeholder="Search..." closable />
+            <flux:command.input placeholder="Search..." closable autofocus />
             <flux:command.items>
                 @foreach($navItems as $item)
                     <flux:command.item icon="{{ $item['icon'] }}" wire:click="goTo('{{ $item['url'] }}')">
